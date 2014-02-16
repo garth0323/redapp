89c82bd28b0ff71490f82991ad8cc2572bdda630
Garth <garth0323@gmail.com>
2014-02-16 00:11:13 -0700

    comment models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212172630) do
+ActiveRecord::Schema.define(version: 20140214035028) do
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
