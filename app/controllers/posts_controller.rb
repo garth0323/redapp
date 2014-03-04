c5a411ebeb33a079603f0c5bdef87b19c80e0517
Garth <garth0323@gmail.com>
2014-03-03 19:45:49 -0700

    add categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,49 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
   	@posts = Post.all
   end
 
   def show
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  	@post.user = User.first #change once authentication in place
+
+  	if @post.save
+  		flash[:notice] = "Your Post was Created"
+  		redirect_to posts_path
+  	else
+  		render :new
+  	end
+  end
+
+  def edit
+  end
+
+  def update
+
+  	if @post.update(post_params)
+  		flash[:notice] = "This Post was Updated"
+  		redirect_to post_path(@post)
+  	else
+  		render :edit
+  	end
+  end
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
   	@post = Post.find(params[:id])
   end
 end
