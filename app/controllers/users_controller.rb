418723e3c9a370fdd5676380963bd3e703ec3222
Garth <garth0323@gmail.com>
2014-03-13 20:48:40 -0700

    user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update]
+	before_action :require_same_user, only: [:edit, :update]
+
+	def show
+	end
+
 	def new
 		@user = User.new
 	end
@@ -15,8 +21,31 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			flash[:notice] = "Updated"
+			redirect_to user_path(@path)
+		else
+			render :edit
+		end
+	end
+
 	def user_params
 		params.require(:user).permit(:username, :password)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+		if current_user != @user
+			flash[:error] = "You can't do that on Redapp"
+			redirect_to root_path
+		end
 	end
 
 end
